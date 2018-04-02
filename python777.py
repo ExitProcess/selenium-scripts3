832f694f13661e7e80db02a5c8e4fdd626a43fb2
ExitProcess <alexeykruglov1@gmail.com>
2018-04-02 12:53:53 +0300

    1

---
+++ python777.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+queries = []
+print("введите запрос и нажмите enter")
+print("после добавления всех слов нажмите enter")
+while True:
+    query = input("запрос: ")
+    if not query:  # если пустая строка то заканчиваем цикл
+        break
+    queries.append(query)
+
+path='C:\SeleniumDrivers\Chrome\chromedriver.exe'
+driver=webdriver.Chrome(path)
+a = 0
+results = dict()
+
+for query in queries:
+    driver.get("https://www.google.ru/")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys(queries[a])
+    elem.send_keys(Keys.RETURN)
+    elem2 = driver.find_element_by_id("resultStats")
+    elem2 = elem2.text
+    results[queries[a]]=elem2
+    a += 1
+
+print(results)
+driver.close()
+driver.quit()
