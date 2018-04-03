cf2bdded639725fe793bb8066f131607a8cbf56d
ExitProcess <alexeykruglov1@gmail.com>
2018-04-03 12:10:13 +0300

    hardcode

--- avito_search.py
+++ avito_search.py
@@ -19,6 +19,7 @@ WebDriverWait(driver, 8).until(
     EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "tab")]//following::label[1]')))
 driver.find_element_by_xpath('//div[contains(@class, "tab")]//following::label[1]').click()
 
+"""цена до 2 000 000 рублей"""
 WebDriverWait(driver, 8).until(
     EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="до, руб."]')))
 driver.find_element_by_xpath('//input[@placeholder="до, руб."]').send_keys("2000000")
@@ -26,5 +27,42 @@ driver.find_element_by_xpath('//input[@placeholder="до, руб."]').send_keys(
 query = driver.find_element_by_id("search")
 query.send_keys("1-к квартира")
 query.send_keys(Keys.RETURN)
+
+"""тип объявления == Продам"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')))
+elem5 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')
+elem5.click()
+#select.Select(elem5).select_by_visible_text("Продам")
+select.Select(elem5).select_by_value("1059")
+
+"""раскрывает список количество комнат"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')))
+elem6 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')
+elem6.click()
+"""отмечает 1 комнатную квартиру"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span')))
+driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span').click()
+
+"""раскрывает список вид объекта"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')))
+elem7 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')
+elem7.click()
+"""отмечает чек-бокс вторичка"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label')))
+driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label').click()
+"""отмечает чек-бокс новостройка"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label')))
+driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label').click()
+
+query2 = driver.find_element_by_id("search")
+query2.clear()
+query2.send_keys("квартира")
+query2.send_keys(Keys.RETURN)
 
 pass
