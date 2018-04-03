6b504ba73a85b678651993272cfc1a141ad19b6e
ExitProcess <alexeykruglov1@gmail.com>
2018-04-03 13:38:43 +0300

    def open_dropdowns

--- avito_search.py
+++ avito_search.py
@@ -14,55 +14,52 @@ elem2 = select.Select(elem).select_by_visible_text("Квартиры")
 elem3 = driver.find_element_by_id("directions")
 elem3.click()
 
-"""label[1] - ленинский, [2] - октябрьский, [3] - пролетарский"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "tab")]//following::label[1]')))
-driver.find_element_by_xpath('//div[contains(@class, "tab")]//following::label[1]').click()
-
-"""цена до 2 000 000 рублей"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="до, руб."]')))
-driver.find_element_by_xpath('//input[@placeholder="до, руб."]').send_keys("2000000")
-
 def search(keys):
     query = driver.find_element_by_id("search")
     query.clear()
     query.send_keys(keys)
     query.send_keys(Keys.RETURN)
 
+dropdown = ("")
+
+def open_dropdowns(xpath):
+    global dropdown
+    WebDriverWait(driver, 8).until(
+        EC.element_to_be_clickable((By.XPATH, xpath)))
+    dropdown = driver.find_element_by_xpath(xpath)
+    dropdown.click()
+
+"""label[1] - ленинский, [2] - октябрьский, [3] - пролетарский"""
+open_dropdowns('//div[contains(@class, "tab")]//following::label[1]')
+
+"""цена до 2 000 000 рублей"""
+WebDriverWait(driver, 8).until(
+    EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="до, руб."]')))
+driver.find_element_by_xpath('//input[@placeholder="до, руб."]').send_keys("2000000")
+
 search("1-к квартира")
 
+"""тип объявления -- раскрыть список"""
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')
+
 """тип объявления == Продам"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')))
-elem5 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')
-elem5.click()
-#select.Select(elem5).select_by_visible_text("Продам")
-select.Select(elem5).select_by_value("1059")
+select.Select(dropdown).select_by_value("1059")    #select.Select(dropdown).select_by_visible_text("Продам")
+
 
 """раскрывает список количество комнат"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')))
-elem6 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')
-elem6.click()
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')
+
 """отмечает 1 комнатную квартиру"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span')))
-driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span').click()
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span')
 
 """раскрывает список вид объекта"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')))
-elem7 = driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')
-elem7.click()
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')
+
 """отмечает чек-бокс вторичка"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label')))
-driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label').click()
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label')
+
 """отмечает чек-бокс новостройка"""
-WebDriverWait(driver, 8).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label')))
-driver.find_element_by_xpath('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label').click()
+open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label')
 
 search("квартира")
 
