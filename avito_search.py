7818d4ff60978d52f4d1aee9181d36577ad00a0c
ExitProcess <alexeykruglov1@gmail.com>
2018-04-03 20:05:16 +0300

    typo

--- avito_search.py
+++ avito_search.py
@@ -14,13 +14,16 @@ elem2 = select.Select(elem).select_by_visible_text("Квартиры")
 elem3 = driver.find_element_by_id("directions")
 elem3.click()
 
+
 def search(keys):
     query = driver.find_element_by_id("search")
     query.clear()
     query.send_keys(keys)
     query.send_keys(Keys.RETURN)
 
+
 dropdown = ("")
+
 
 def open_dropdowns(xpath):
     global dropdown
@@ -29,36 +32,36 @@ def open_dropdowns(xpath):
     dropdown = driver.find_element_by_xpath(xpath)
     dropdown.click()
 
-"""label[1] - ленинский, [2] - октябрьский, [3] - пролетарский"""
+
+# label[1] - ленинский, [2] - октябрьский, [3] - пролетарский
 open_dropdowns('//div[contains(@class, "tab")]//following::label[1]')
 
-"""цена до 2 000 000 рублей"""
+# цена до 2 000 000 рублей
 WebDriverWait(driver, 8).until(
     EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="до, руб."]')))
 driver.find_element_by_xpath('//input[@placeholder="до, руб."]').send_keys("2000000")
 
 search("1-к квартира")
 
-"""тип объявления -- раскрыть список"""
+# тип объявления -- раскрыть список
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[1]/div/select')
 
-"""тип объявления == Продам"""
-select.Select(dropdown).select_by_value("1059")    #select.Select(dropdown).select_by_visible_text("Продам")
+# тип объявления == Продам
+select.Select(dropdown).select_by_value("1059")  # select.Select(dropdown).select_by_visible_text("Продам")
 
-
-"""раскрывает список количество комнат"""
+# раскрывает список количество комнат
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div/span')
 
-"""отмечает 1 комнатную квартиру"""
+# отмечает 1 комнатную квартиру
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/ul/li[2]/label/span')
 
-"""раскрывает список вид объекта"""
+# раскрывает список вид объекта
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div')
 
-"""отмечает чек-бокс вторичка"""
+# отмечает чек-бокс вторичка
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[1]/label')
 
-"""отмечает чек-бокс новостройка"""
+# отмечает чек-бокс новостройка
 open_dropdowns('//*[@id="catalog"]/div[4]/div/div/div/div/div[3]/div/div/div[2]/div/div/div/ul/li[2]/label')
 
 search("квартира")
