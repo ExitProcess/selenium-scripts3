b30472601401529f1323d1626503bceaeb0bdeaa
ExitProcess <alexeykruglov1@gmail.com>
2018-04-04 23:04:27 +0300

    test

---
+++ sliders_tests.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
+driver = webdriver.Chrome(path)
+driver.get("https://soundcloud.com/user-95923847/5-1")
