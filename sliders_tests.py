c6d5b0ecb10224a94281fb7424e67a40636b5efe
ExitProcess <alexeykruglov1@gmail.com>
2018-04-05 14:50:18 +0300

    new

--- sliders_tests.py
+++ sliders_tests.py
@@ -1,5 +1,6 @@
 # тест слайдера
-# пока что включает бабангиду и фиксирует курсор на ползунке громкости
+# включает бабангиду и фиксирует курсор на ползунке громкости
+# в зависимости от времени воспроизведения, прибавляет громкость
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,25 +9,53 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get("https://soundcloud.com/user-95923847/5-1")
 
+# закрывает сообщение о кукисах
 cookie_close = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                                         ".announcement__dismiss")))
 cookie_close.click()
 
+# ставит трек на рипит
 repeat = driver.find_element_by_xpath("//button[@title='Repeat']")
 repeat.click()
 
+# наводит курсор на иконку громкости, чтобы появился ползунок
 volume_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
                                                  "//div[@class='volume__iconWrapper']")))
 ActionChains(driver).move_to_element(volume_button).perform()
 
-slider = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
-                                            "//div[@class='volume__sliderHandle']")))
+#slider = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
+#                                            "//div[@class='volume__sliderHandle']")))
+#ActionChains(driver).move_to_element(slider).perform()
+#ActionChains(driver).click_and_hold(slider).perform()
 
-ActionChains(driver).move_to_element(slider).perform()
-ActionChains(driver).click_and_hold(slider).perform()
+# с начала воспроизведения громкость устанавливается на 50%
+ActionChains(driver).key_down(Keys.LEFT_SHIFT).send_keys(Keys.ARROW_DOWN).perform()
+ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+
+# цикл проверяет время воспроизведения трека
+# громкость на soundcloud регулируется shift+up, shift+down
+# громкость установлена на 50%, на отметке 0:10 установится на 90%
+seconds = 300
+while seconds > 1:
+    current_time = driver.find_element_by_css_selector('.playbackTimeline__timePassed [aria-hidden]')
+    if current_time.text == "0:10":
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+        ActionChains(driver).key_up(Keys.LEFT_SHIFT).perform()
+        break
+    print(current_time.text)
+    seconds -= 1
+    time.sleep(1)
+
 pass
