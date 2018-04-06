848ba753c3e1c7575e789af2bdab469aaaf979f7
ExitProcess <alexeykruglov1@gmail.com>
2018-04-06 13:16:15 +0300

    fix 2

--- soundcloud_manager.py
+++ soundcloud_manager.py
@@ -89,7 +89,7 @@ def manager(x):
         repeat_all()
     elif user == "repeat none":  # 0
         repeat_none()
-    elif str.find(user, "0") and (len(user) > 1):  # если в строке есть ноль и строка больше 1 (защита от дурака)
+    elif str.find(user, "0"): # and (len(user) > 1):  # если в строке есть ноль и строка больше 1 (защита от дурака)
         volume = int(user)  # преобразовываем в целое (пока нет полной защиты от дурака, например от play0, 000 и т.д.)
         if volume < 0:
             volume = -volume
@@ -120,7 +120,6 @@ user = ""
 while user != "close":
     user = input("запрос: ")
     manager(user)
-    break
 
 driver.close()
 driver.quit()
