d9fe5163caf7f7d9744fd7ee792640987c3eb2a1
ExitProcess <alexeykruglov1@gmail.com>
2018-04-08 19:44:18 +0300

    new

---
+++ bbk_comparsion_2models.py
@@ -0,0 +1,65 @@
+# скрипт заходит на сайт bbk, выбирает категорию "тв-ресиверы"
+# затем парсит страницу с моделями, создает список всех элементов tag a, то есть cписок ссылок
+# потом создается второй список, куда входят только ссылки на страницы моделей ресиверов
+# из второго списка выбираются нужные нам модели, а именно 'SMP145HDT2' и 'SMP240HDT2'
+# затем поочередно открываются страницы моделей, каждая добавляется в сравнение, после чего открывается страницы сравнения
+# cкрипт неоптимизирован!!!
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
+driver = webdriver.Chrome(path)
+driver.get("http://www.bbk.ru/")
+
+prod_button = WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH,
+                                                                         "//a[@href='/production/']")))
+ActionChains(driver).move_to_element(prod_button).perform()
+
+recievers_button = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH,
+                                                                              "//a[@href='/production/tv_video/tv_receivers/']")))
+ActionChains(driver).click(recievers_button).perform()
+
+model_list = [] # список элементов моделей
+link_list = driver.find_elements_by_tag_name("a") # список всех элементов со ссылками
+for i in link_list: # выбираем только элементы моделей, добавляем в model_list
+    if len(i.text) == 10:
+        model_list.append(i)
+print(model_list)
+
+a = "145"
+b = "240"
+a_link = ""
+b_link = ""
+# 145, 240
+# 'SMP145HDT2', 'SMP240HDT2'
+
+for i in model_list:
+    if i.text[3:6] == a:
+        print("найдена ссылка на модель ", a)
+        a_link = i.get_attribute('href')
+        print(a_link)
+    if i.text[3:6] == b:
+        print("найдена ссылка на модель ", b)
+        b_link = i.get_attribute('href')
+        print(b_link)
+
+driver.get(a_link)
+add_to_comp = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                              ".add_to_comparsion")))
+add_to_comp.click()
+driver.get(b_link)
+add_to_comp = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                              ".add_to_comparsion")))
+add_to_comp.click()
+
+comparsion = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                              ".compare")))
+comparsion.click()
+
+comparsion2 = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                              ".compare_link")))
+comparsion2.click()
