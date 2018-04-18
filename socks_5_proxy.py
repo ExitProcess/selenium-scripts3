88ef1a912aad34cec09bb4bab7dd950173626474
ExitProcess <alexeykruglov1@gmail.com>
2018-04-19 02:03:35 +0300

    убрал вывод номера строки, добавил комменты

--- socks_5_proxy.py
+++ socks_5_proxy.py
@@ -20,22 +20,24 @@ sort_all.click()
 select.Select(sort_all).select_by_value("5")
 
 # теперь парсим строки, выбираем сервера со 100% аптаймом
-# если аптайм сервака == 100%, то выводим на печать номер строки, ip:port, страну, аптайм сервака + количество проверок
+# если аптайм сервака == 100%, то выводим на печать ip:port, страну, аптайм сервака + (количество проверок)
 # "//tbody/tr[4]" - "//tbody/tr[503]" -- столько всего строк
 for str_count in range(4, 503):
-
+    # локатор аптайма
     percents_xpath = "//tr[" + str(str_count) + "]/td[8]"
     percent_elem = driver.find_element_by_xpath(percents_xpath)
 
     if percent_elem.text[0:3] == "100":
-
+        # локатор ip:port + получение индекса начала текста ip:port
         ip_port_xpath = "// tr[" + str(str_count) + "] / td[1]"
         ip_port_elem = driver.find_element_by_xpath(ip_port_xpath)
+        ip_port_clear = ip_port_elem.text
+        index = ip_port_clear.rfind(" ")
 
         country_xpath = "// tr[" + str(str_count) + "] / td[5]"
         country_elem = driver.find_element_by_xpath(country_xpath)
 
-        print(ip_port_elem.text, country_elem.text, percent_elem.text)
+        print(ip_port_clear[index+1:], country_elem.text, percent_elem.text)
 
 driver.close()
 driver.quit()
