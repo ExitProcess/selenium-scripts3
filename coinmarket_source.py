5c7ec3c353ddb85512a755539db95be5f4664388
ExitProcess <alexeykruglov1@gmail.com>
2018-05-05 20:24:19 +0300

    new

---
+++ coinmarket_source.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from lxml import html
+
+path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
+driver = webdriver.Chrome(path)
+
+driver.get("https://coinmarketcap.com/all/views/all/")
+
+execute_time = time.time()
+
+source = driver.page_source
+tree = html.fromstring(source)
+
+num_list = tree.xpath("//tr/td[1]") # \n1\n
+name_list = tree.xpath("//tr/td[2]")
+# symbol_list = tree.xpath("//tr/td[3]") # ADA
+mcap_list = tree.xpath("//tr/td[4]") # '$9 649 627 724'
+
+index = 0
+for num in num_list:
+    num = num.text[1:-1] # \n1\n -->> 1
+
+    name_str = name_list[index].text_content() # \n\nXRP\nRiple\n
+    name_str = name_str.strip("\n") # XRP\nRiple
+    str_divide = name_str.find('\n')
+
+    name = name_str[str_divide+1:]
+    symbol = name_str[:str_divide]
+
+    mcap = mcap_list[index].text
+
+    print(num, name, symbol, mcap)
+    index += 1
+
+execute_time = time.time() - execute_time
+print(execute_time)
