e4130b1d0d15bfb49a2fdd50748e4857b10de91b
ExitProcess <alexeykruglov1@gmail.com>
2018-05-11 15:46:59 +0300

    refactor

--- coinmarket_sort_market_cap.py
+++ coinmarket_sort_market_cap.py
@@ -7,19 +7,24 @@ path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
 
 driver.get("https://coinmarketcap.com/all/views/all/")
+# сортировка по убыванию
 sort_button = driver.find_element_by_css_selector("#th-marketcap")
 sort_button.click()
 
 #
-
 list_mc = driver.find_elements_by_css_selector(".market-cap")
 del list_mc[-1]
 
-
 for i in range(0, len(list_mc)-1):
-    if list_mc[i].text > list_mc[i + 1].text:
-        print(list_mc[i].text, "больше", list_mc[i + 1].text)
-    if list_mc[i].text == "$?" and len(list_mc[i-1].text) > 2:
-        print(list_mc[i-1].text, "последний элемент с известной стоимостью")
-    if list_mc[i].text == "$?":
-        print(list_mc[i].text, "стоимость неизвестна")
+    elem_current = list_mc[i].text
+    elem_next = list_mc[i+1].text
+    elem_prev = list_mc[i-1].text
+    if elem_current > elem_next:
+        print(elem_current, "больше", elem_next)
+    if elem_current == "$?" and len(elem_prev) > 2:
+        print(elem_prev, "последний элемент с известной стоимостью")
+    if elem_current == "$?":
+        print(elem_current, "стоимость неизвестна")
+
+driver.close()
+driver.quit()
