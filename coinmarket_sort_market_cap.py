4b1fe22367ff0262648e0d1536970bd4cc3b0793
ExitProcess <alexeykruglov1@gmail.com>
2018-05-12 14:21:08 +0300

    fix

--- coinmarket_sort_market_cap.py
+++ coinmarket_sort_market_cap.py
@@ -20,7 +20,7 @@ for i in range(0, len(list_mcap_elements) - 1):
     elem_next = list_mcap_elements[i+1].text
     elem_prev = list_mcap_elements[i-1].text
     # условие для сравнения строк разной длины ("$146 109 484 586 больше $67 635 761 404")
-    if len(elem_current) > len(elem_next) and len(elem_next) != 2:
+    if len(elem_current) > len(elem_next) > 2:
         print(elem_current, "больше", elem_next)
     # условие для сравнения строк одинаковой длины ("$67 635 761 404 больше $27 646 445 524")
     if elem_current > elem_next:
