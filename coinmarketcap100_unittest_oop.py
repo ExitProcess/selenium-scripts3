246bae36756fefd87b43fefd2078ae7b34391523
ExitProcess <alexeykruglov1@gmail.com>
2018-05-17 12:40:49 +0300

    fix

--- coinmarketcap100_unittest_oop.py
+++ coinmarketcap100_unittest_oop.py
@@ -21,11 +21,8 @@ class CoinMarketCapSort(unittest.TestCase):
         for i in range(0, len(list_mcap_elements_dec) - 1):  # последний элемент списка надо выводить отдельно
             elem_current = list_mcap_elements_dec[i].text
             elem_next = list_mcap_elements_dec[i + 1].text
-            # условие для сравнения строк разной длины ("$146 109 484 586 больше $67 635 761 404")
-            if len(elem_current) > len(elem_next):
-                count += 1
-            # условие для сравнения строк одинаковой длины ("$67 635 761 404 больше $27 646 445 524")
-            if elem_current > elem_next:
+            # if "$1 000" больше "$100" or "$2 000" больше "$1 000"
+            if len(elem_current) > len(elem_next) or elem_current > elem_next:
                 count += 1
         elem_last = list_mcap_elements_dec[-1].text
         if elem_last < elem_current or len(elem_last) < len(elem_current):
@@ -48,16 +45,13 @@ class CoinMarketCapSort(unittest.TestCase):
         for i in range(0, len(list_mcap_elements_inc) - 1):  # последний элемент списка надо выводить отдельно
             elem_current = list_mcap_elements_inc[i].text
             elem_next = list_mcap_elements_inc[i + 1].text
-            # условие для сравнения строк разной длины ("$67 635 761 404" меньше "$146 109 484 586")
-            if len(elem_current) < len(elem_next):
-                count += 1
-            # условие для сравнения строк одинаковой длины ("$27 646 445 524" больше "$67 635 761 404")
-            if elem_current < elem_next:
+            # if "$100" < "$1 000" or "$1 000" < "$2 000"
+            if len(elem_current) < len(elem_next) or elem_current < elem_next:
                 count += 1
         elem_last = list_mcap_elements_inc[-1].text
         if elem_last > elem_current or len(elem_last) > len(elem_current):
             count += 1
-        # проверено 100 элементов, если счетчик == 100, то все элементы расположены в порядке убывания
+        # проверено 100 элементов, если счетчик == 100, то все элементы расположены в порядке возрастания
         assert count == 100
 
     def tearDown(self):
