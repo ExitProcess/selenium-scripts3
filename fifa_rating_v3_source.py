bef017dc96ea7184899a13427fcb8944f6d60c34
ExitProcess <alexeykruglov1@gmail.com>
2018-05-25 12:45:17 +0300

    fix import

--- fifa_rating_v3_source.py
+++ fifa_rating_v3_source.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
