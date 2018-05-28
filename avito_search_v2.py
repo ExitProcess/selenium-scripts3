9e9434dff8f34a6fceb3f63f1d7f255dc6ae14f2
ExitProcess <alexeykruglov1@gmail.com>
2018-05-28 22:44:03 +0300

    pep8, comments, import etc

--- avito_search_v2.py
+++ avito_search_v2.py
@@ -1,10 +1,13 @@
+# один из самых первых скриптов
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import select
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
@@ -15,25 +18,30 @@ elem2 = select.Select(elem).select_by_visible_text("Автомобили")
 elem3 = driver.find_element_by_id("directions")
 elem3.click()
 
+
 # клик и раскрытие выпадающего меню
 def open_dropdowns(xpath):
     global dropdown, dropdown2
     WebDriverWait(driver, 12).until(
-        EC.element_to_be_clickable((By.XPATH, xpath)))
+        ec.element_to_be_clickable((By.XPATH, xpath)))
     dropdown = dropdown2 = driver.find_element_by_xpath(xpath)
     dropdown.click()
+
 
 # клик и удержание по кнопке слайдера
 def slider_hold(slider):
     ActionChains(driver).click_and_hold(slider).perform()
 
+
 # перемещение слайдера
-def slider_move(offsetX, offsetY):
-    ActionChains(driver).move_by_offset(offsetX, offsetY).perform()
+def slider_move(offset_x, offset_y):
+    ActionChains(driver).move_by_offset(offset_x, offset_y).perform()
+
 
 # перестает удерживать кнопку слайдера
 def slider_release(slider):
     ActionChains(driver).release(slider).perform()
+
 
 # label[1] - ленинский, [2] - октябрьский, [3] - пролетарский
 open_dropdowns('//div[contains(@class, "tab")]//following::label[1]')
