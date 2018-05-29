91be3a685a57abce75ca6eeba92a45080114d40e
ExitProcess <alexeykruglov1@gmail.com>
2018-05-30 00:33:48 +0300

    new script

---
+++ test_fibo.py
@@ -0,0 +1,16 @@
+def fibo(user):
+    global b
+    a, b = 0, 1
+    # > 2 для ряда начинающегося с 0, > 1 для ряда начинающегося с 1
+    while user > 1:
+        a, b = b, a + b
+        user -= 1
+    return b
+
+
+user = -1
+while user != 0:
+    user = input("n: ")
+    user = int(user)
+    fibo(user)
+    print(b)
