9ccc71a912b722a82e42574501bf6e6d0156b802
ExitProcess <alexeykruglov1@gmail.com>
2018-07-12 02:59:10 +0300

    мелкие правки по pep8

--- bbk_comparsion_2models.py
+++ bbk_comparsion_2models.py
@@ -2,32 +2,34 @@
 # затем парсит страницу с моделями, создает список всех элементов tag a, то есть cписок ссылок
 # потом создается второй список, куда входят только ссылки на страницы моделей ресиверов
 # из второго списка выбираются нужные нам модели, а именно 'SMP145HDT2' и 'SMP240HDT2'
-# затем поочередно открываются страницы моделей, каждая добавляется в сравнение, после чего открывается страницы сравнения
+# затем поочередно открываются страницы моделей, каждая добавляется в сравнение,
+# после чего открывается страница сравнения
 # cкрипт неоптимизирован!!! в скрипте используется метод получения всех ссылок на странице
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 
 path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get("http://www.bbk.ru/")
 
-prod_button = WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH,
+prod_button = WebDriverWait(driver, 7).until(ec.element_to_be_clickable((By.XPATH,
                                                                          "//a[@href='/production/']")))
 ActionChains(driver).move_to_element(prod_button).perform()
 
-recievers_button = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH,
-                                                                              "//a[@href='/production/tv_video/tv_receivers/']")))
+recievers_button = WebDriverWait(driver, 4).until(ec.element_to_be_clickable((By.XPATH,
+                                                                              "//a[@href='/production/tv_video"
+                                                                              "/tv_receivers/']")))
 ActionChains(driver).click(recievers_button).perform()
 
-model_list = [] # список элементов моделей
+model_list = []  # список элементов моделей
 # получаем все ссылки на странице
-link_list = driver.find_elements_by_tag_name("a") # список всех элементов со ссылками
+link_list = driver.find_elements_by_tag_name("a")  # список всех элементов со ссылками
 # отбираем только ссылки, ведущие на модели ресиверов
-for i in link_list: # выбираем только элементы моделей, добавляем в model_list
+for i in link_list:  # выбираем только элементы моделей, добавляем в model_list
     if len(i.text) == 10:
         model_list.append(i)
 print(model_list)
@@ -50,18 +52,18 @@ for i in model_list:
         print(b_link)
 
 driver.get(a_link)
-add_to_comp = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                              ".add_to_comparsion")))
+add_to_comp = WebDriverWait(driver, 4).until(ec.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                         ".add_to_comparsion")))
 add_to_comp.click()
 driver.get(b_link)
-add_to_comp = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                              ".add_to_comparsion")))
+add_to_comp = WebDriverWait(driver, 4).until(ec.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                         ".add_to_comparsion")))
 add_to_comp.click()
 
-comparsion = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                              ".compare")))
+comparsion = WebDriverWait(driver, 4).until(ec.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                        ".compare")))
 comparsion.click()
 
-comparsion2 = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                              ".compare_link")))
+comparsion2 = WebDriverWait(driver, 4).until(ec.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                         ".compare_link")))
 comparsion2.click()
