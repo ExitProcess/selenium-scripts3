33a23f45a2f9db6a6793e927971c50a34ab853be
ExitProcess <alexeykruglov1@gmail.com>
2018-07-18 04:27:35 +0300

    small optimization

--- absolute_links_requests_html.py
+++ absolute_links_requests_html.py
@@ -17,15 +17,15 @@ links_set = response3.html.absolute_links
 # фильтр -- ссылки только на www.python.org, не добавлять в список:
 # подсайты и сторонние сайты ('https://docs.python.org/3/', 'http://twitter.com/ThePSF')
 # ссылки с якорями ('https://www.python.org/about/success/#government')
-# обрабатываются дубли и одинаковые ссылки со слешем и без ('https://www.python.org/', 'https://www.python.org')
+# НЕ НАДО обрабатывать дубли и одинаковые ссылки со слешем и без ('https://www.python.org/', 'https://www.python.org'),
+# т.к. это уже делает метод absolute_links
 clear_list = []
 for link in links_set:
     if "www.python.org" in link and "#" not in link:
-        if link not in clear_list and (link + '/') not in clear_list and link[:-1] not in clear_list:
-            clear_list.append(link)
+        clear_list.append(link)
 
 finish = time.time() - start
 
 print(clear_list)
 print(len(clear_list))  # 83
-print(finish)  # 0.9390535354614258 - 1.0250585079193115
+print(finish)  # 0.9380536079406738 - 1.0310587882995605
