4fd8bf2d16ff53c4790fa4f7138f443b3199dfce
ExitProcess <alexeykruglov1@gmail.com>
2018-07-24 18:33:32 +0300

    small refactoring

--- link_test_v2.py
+++ link_test_v2.py
@@ -4,7 +4,6 @@
 import time
 import urllib.error
 import urllib.request
-# from urllib.request import urlopen
 from lxml import html
 
 links_base = ["https://www.python.org/", ]
@@ -18,6 +17,7 @@ doc.close()
 while index != len(links_base):
     try:
         response = urllib.request.urlopen(links_base[index])
+        # обработка переадресации на подсайт
         if "www.python.org" not in response.geturl():
             index += 1
             continue
@@ -42,15 +42,15 @@ while index != len(links_base):
         # обработка результатов -- ссылки только на www.python.org, не добавлять в список:
         # подсайты и сторонние сайты ('https://docs.python.org/3/', 'http://twitter.com/ThePSF')
         # ссылки с якорями ('https://www.python.org/about/success/#government')
-        # обрабатываются дубли и одинаковые ссылки со слешем и без ('https://www.python.org/', 'https://www.python.org')
         for link in links_list:
             if "ftp" not in link and "#" not in link and "www.python.org" in link:
-                if link not in links_base and (link + '/') not in links_base and link[:-1] not in links_base:
-                    # для того, чтобы работал метод make_links_absolute, ссылка должна заканчиваться слешем
-                    # "?" not in link - https://www.python.org/jobs/?page=1 -- не добавлять, т.к. запрос
-                    # не добавлять слеш к расширениям -- .asc, .txt, .png, .psd, .svg, .pdf
-                    if link[-1] != "/" and link[-4] != "." and link[-5] != "." and "?" not in link:
+                # для того, чтобы работал метод make_links_absolute, ссылка должна заканчиваться слешем
+                # "?" not in link - https://www.python.org/jobs/?page=1 -- не добавлять слеш, т.к. запрос
+                # не добавлять слеш к расширениям -- .asc, .txt, .png, .psd, .svg, .pdf, .html и т.д.
+                if link[-1] != "/":
+                    if link[-4] != "." and link[-5] != "." and "?" not in link:
                         link = link + "/"
+                if link not in links_base:
                     links_base.append(link)
                     referer_link.append(links_base[index])
     index += 1
