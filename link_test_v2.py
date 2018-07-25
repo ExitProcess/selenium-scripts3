90d0d7c77ba05fdc2697c66f698af855e64fb510
ExitProcess <alexeykruglov1@gmail.com>
2018-07-26 01:54:29 +0300

    + работа через прокси + обработка UnicodeEncodeError

--- link_test_v2.py
+++ link_test_v2.py
@@ -1,10 +1,32 @@
 # еще один линк-чекер, в отличие от первой версии, которая была реализована на библиотеке selenium webdriver,
 # во второй версии используются библиотеки urllib и lxml
 
-import time
 import urllib.error
 import urllib.request
 from lxml import html
+
+# список прокси
+proxy_index = -1
+proxies = ['91.234.183.112:41258',
+           '5.175.75.190:8080',
+           '91.224.63.218:8080',
+           '168.181.212.101:8080',
+           '212.69.18.132:36029',
+           '178.63.8.211:3128',
+           '46.50.136.4:53281',
+           '46.45.19.138:53281',
+           '2.94.171.75:8080',
+           '178.33.194.169:3128',
+           '89.189.130.205:8080',
+           '46.227.161.214:53281',
+           '188.120.231.22:10009',
+           '81.163.61.21:41258',
+           '178.176.28.164:8080',
+           '109.74.142.138:53281',
+           '77.79.146.91:3128',
+           '193.105.124.127:8080',
+           '91.201.169.243:41258'
+           ]
 
 links_base = ["https://www.python.org/", ]
 referer_link = ["https://www.python.org/", ]
@@ -21,6 +43,8 @@ while index != len(links_base):
         if "www.python.org" not in response.geturl():
             index += 1
             continue
+    except UnicodeEncodeError:  # обработка unicode-ссылок, пока заглушка, позже будет нормальный обработчик
+        print(links_base[index])
     except urllib.error.HTTPError as error:
         if error.code == 404:
             doc = open("404.txt", "a+")
@@ -28,11 +52,16 @@ while index != len(links_base):
             doc.write((" " * 8) + links_base[index] + "\n")
             doc.close()
             count_404 += 1
-    except urllib.error.URLError as error:
+    except urllib.error.URLError as error:  # обычно [WinError 10060]
         print(error.reason)
-        print("pause")
-        time.sleep(30)
-        continue
+        proxy_index += 1  # (первый или) следующий прокси из списка
+        try:
+            proxy_support = urllib.request.ProxyHandler({'https': proxies[proxy_index]})
+            opener = urllib.request.build_opener(proxy_support)
+            urllib.request.install_opener(opener)
+            continue
+        except Exception:  # если не удалось подключиться к прокси, то повторить запрос с предыдущим подключением
+            continue
     else:
         tree = html.fromstring(response.read())
         tree.make_links_absolute(response.geturl(), resolve_base_href=False)
