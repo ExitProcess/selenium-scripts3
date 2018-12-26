1c2e897c6464a26fdfdfa10fa8864e5a8784f92c
ExitProcess <alexeykruglov1@gmail.com>
2018-12-27 01:04:22 +0300

    comments

--- rambler_news_cat_v2.py
+++ rambler_news_cat_v2.py
@@ -3,14 +3,17 @@ from selenium import webdriver
 path = 'C:\SeleniumDrivers\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(path)
 
+# локаторы
 css_selectors = ("[data-blocks='finance\:\:not_current']",
                  "[data-blocks='woman\:\:not_current']",
                  "[data-blocks='kino\:\:not_current']")
 
+# ссылки
 urls = ("https://finance.rambler.ru/",
         "https://woman.rambler.ru/",
         "https://kino.rambler.ru/")
 
+# заголовки
 titles = ("Рамблер/финансы",
           "Рамблер/женский",
           "Рамблер/кино")
@@ -24,7 +27,9 @@ index = 0
 while index < len(css_selectors):
     cat_button = driver.find_element_by_css_selector(css_selectors[index])
     cat_button.click()
+    # соответствие категории и ссылки
     assert urls[index] in driver.current_url
+    # соответствие категории и заголовка
     assert titles[index] in driver.title
     index += 1
 
